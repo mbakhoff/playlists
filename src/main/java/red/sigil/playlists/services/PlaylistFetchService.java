eaf45e9fcea44ac6388fe2ac2185d149f6336574
Märt Bakhoff <mart.bakhoff@gmail.com>
2017-06-09 20:46:23 +0300

    drop youtube api

--- src/main/java/red/sigil/playlists/services/PlaylistFetchService.java
+++ src/main/java/red/sigil/playlists/services/PlaylistFetchService.java
@@ -1,15 +1,19 @@
 package red.sigil.playlists.services;
 
-import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
-import com.google.api.client.json.jackson2.JacksonFactory;
-import com.google.api.services.youtube.YouTube;
-import com.google.api.services.youtube.model.Playlist;
-import com.google.api.services.youtube.model.PlaylistItem;
-import com.google.api.services.youtube.model.PlaylistItemListResponse;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.http.StatusLine;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.utils.URIBuilder;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -19,57 +23,88 @@ public class PlaylistFetchService {
   @Autowired
   private PropertyService propertyService;
 
-  private YouTube youtube;
+  private static final String PLAYLISTS =
+      "https://www.googleapis.com/youtube/v3/playlists";
+  private static final String PLAYLIST_ITEMS =
+      "https://www.googleapis.com/youtube/v3/playlistItems";
+
+  private CloseableHttpClient httpclient;
+  private ObjectMapper mapper;
   private String apiKey;
 
   @PostConstruct
   public void init() throws Exception {
     apiKey = propertyService.getProperty("yt-apikey");
-    youtube = new YouTube(
-        GoogleNetHttpTransport.newTrustedTransport(),
-        JacksonFactory.getDefaultInstance(),
-        req -> { });
+    httpclient = HttpClients.createDefault();
+    mapper = new ObjectMapper();
+  }
+
+  @PreDestroy
+  public void destroy() throws Exception {
+    httpclient.close();
   }
 
   public ItemInfo read(String playlistId) throws Exception {
-    List<Playlist> items = youtube.playlists().list("id,snippet")
-        .setKey(apiKey)
-        .setId(playlistId)
-        .execute()
-        .getItems();
-    return items.isEmpty() ? null : new ItemInfo(items.get(0));
+    HttpGet httpGet = new HttpGet(new URIBuilder(PLAYLISTS)
+        .addParameter("part", "id,snippet")
+        .addParameter("id", playlistId)
+        .addParameter("key", apiKey)
+        .build());
+
+    try (CloseableHttpResponse resp = httpclient.execute(httpGet)) {
+      StatusLine status = resp.getStatusLine();
+      if (status.getStatusCode() != 200)
+        throw new IOException(status.getReasonPhrase());
+
+      JsonNode root = mapper.readTree(resp.getEntity().getContent());
+      JsonNode items = root.at("/items");
+      if (items.size() == 0)
+        return null;
+
+      JsonNode playlist = items.get(0);
+      return new ItemInfo(
+          playlist.at("/id").asText(),
+          playlist.at("/snippet/title").asText());
+    }
   }
 
   public List<ItemInfo> readItems(String playlistId) throws Exception {
     List<ItemInfo> playlist = new ArrayList<>();
     String page = null;
     do {
-      PlaylistItemListResponse result = youtube.playlistItems().list("id,snippet,contentDetails")
-          .setKey(apiKey)
-          .setPlaylistId(playlistId)
-          .setMaxResults(50L)
-          .setPageToken(page)
-          .execute();
-      for (PlaylistItem item : result.getItems()) {
-        playlist.add(new ItemInfo(item));
+      HttpGet httpGet = new HttpGet(new URIBuilder(PLAYLIST_ITEMS)
+          .addParameter("part", "id,snippet,contentDetails")
+          .addParameter("playlistId", playlistId)
+          .addParameter("maxResults", "50")
+          .addParameter("pageToken", page)
+          .addParameter("key", apiKey)
+          .build());
+
+      try (CloseableHttpResponse resp = httpclient.execute(httpGet)) {
+        StatusLine status = resp.getStatusLine();
+        if (status.getStatusCode() != 200)
+          throw new IOException(status.getReasonPhrase());
+
+        JsonNode root = mapper.readTree(resp.getEntity().getContent());
+        page = root.at("/nextPageToken").asText(null);
+        for (JsonNode playlistItem : root.at("/items")) {
+          playlist.add(new ItemInfo(
+              playlistItem.at("/contentDetails/videoId").asText(),
+              playlistItem.at("/snippet/title").asText()));
+        }
       }
-      page = result.getNextPageToken();
     } while (page != null);
     return playlist;
   }
 
   public static class ItemInfo {
+
     public final String id;
     public final String title;
 
-    public ItemInfo(Playlist playlist) {
-      this.id = playlist.getId();
-      this.title = playlist.getSnippet().getTitle();
-    }
-
-    public ItemInfo(PlaylistItem item) {
-      this.id = item.getContentDetails().getVideoId();
-      this.title = item.getSnippet().getTitle();
+    public ItemInfo(String id, String title) {
+      this.id = id;
+      this.title = title;
     }
   }
 }
